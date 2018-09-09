57f13e7a715b0a4d5681d04e5968e51fe2d7dd97
Miguel Pimentel <miguelpimentel2013@gmail.com>
2018-09-09 14:11:57 -0300

    Applied new structure of Map Tree

--- main.hs
+++ main.hs
@@ -3,14 +3,5 @@
     Description : Delegated to execute all modules
 -}
 
-
-data Tree a = Null
-          | Node a (Tree a) (Tree a) deriving(Show)
-
 main = do
       putStrLn "Hello, world!"
-      putStrLn "Miguel"
-
-      let tree = Node 5 (Node 3 Null Null) (Node 2 Null Null)
-      print tree
-   
