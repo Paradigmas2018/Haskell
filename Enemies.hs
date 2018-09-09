758734a82985f9a8d7f2bf09b2d31e41f339b68a
vitorandos <vicios@MacBook-Pro-de-Vitor.local>
2018-09-09 16:59:30 -0300

    Creating enemies

---
+++ Enemies.hs
@@ -0,0 +1,38 @@
+module Enemies
+    (
+    ramsor,
+    mynxe,
+    anmi,
+    ghaaaauya,
+    lohn,
+    mountainWolf,
+    malfoy,
+    robinhoODoFilme,
+    singleEyedGladiator,
+    itzel,
+    hydell,
+    durgess,
+    terak,
+    ily,
+    nox,
+    kingForgusGhost
+    ) where
+
+import Unit
+
+ramsor = createUnit "Ramsor" Warrior 5 5 5 5 False
+mynxe = createUnit "Mynx" Wizard 10 9 2 12 False
+anmi = createUnit "Anmi" Archer 8 6 4 4 False
+ghaaaauya = createUnit "Ghaaaauya" Warrior 4 4 3 3 False
+lohn = createUnit "Lohn" Wizard 7 2 9 3 False
+mountainWolf = createUnit "Lobo da Montanha" Warrior 2 9 5 5 False
+malfoy = createUnit "Malfoy" Wizard 8 8 4 9 False
+robinhoODoFilme = createUnit "Robinho o do Filme" Archer 7 5 9 11 False
+singleEyedGladiator = createUnit "Gladiador de um Só Olho" Warrior 14 10 2 4 False
+itzel = createUnit "Itzel" Warrior 6 12 5 1 False
+hydell = createUnit "Hydell" Archer 8 9 3 4 False
+durgess = createUnit "Durgess" Wizard 1 1 1 1 False
+terak = createUnit "Terak" Warrior 15 10 6 6 False
+ily = createUnit "Ily" Archer 3 3 9 5 False
+nox = createUnit "Nox" Wizard 6 3 9 4 False
+kingForgusGhost = createUnit "Fantasma do Rei Forgus" Warrior 4 8 2 1 False
--- README.md
+++ README.md
@@ -7,16 +7,15 @@ Projeto criado com intuito acadêmico. Objetiva-se desenvolver conceitos de ling
 ## 2. Membros
 
 * Andrew Lucas - 16/0023921
-* João Egewarth -
 * Miguel Pimentel - 14/0156143
-* Vitor Gomes  -
+* Vitor Gomes  - 15/0048106
 
 ## 3. Executando
 
 * Criar executável:
 
 ````
-  make 
+  make
 ````
 
 * Executar:
