e031a7b214e848e129bad8881bc76dd659b1be44
Miguel Pimentel <miguelpimentel2013@gmail.com>
2018-09-09 10:55:54 -0300

    Updating Tree structure

--- main.hs
+++ main.hs
@@ -4,5 +4,13 @@
 -}
 
 
+data Tree a = Null
+          | Node a (Tree a) (Tree a) deriving(Show)
+
 main = do
       putStrLn "Hello, world!"
+      putStrLn "Miguel"
+
+      let tree = Node 5 (Node 3 Null Null) (Node 2 Null Null)
+      print tree
+   
